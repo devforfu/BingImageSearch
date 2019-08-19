e4a692844cdb25e4d09351a7ace6da73494643ba
devforfu <developer.z@outlook.com>
2019-08-19 20:55:27 +0200

    Moving workers into a separate file

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -5,32 +5,37 @@ import (
     "bing/io"
     "bing/utils"
     "encoding/json"
-    "fmt"
     "io/ioutil"
     "log"
     "os"
     "path"
     "sync"
-    "time"
 )
 
+// Crawler takes Bing API client instance and sends queries to the search engine.
+// It also responsible for taking URLs from search results and using them to
+// download the images into a local folder.
 type Crawler struct {
     Client *api.BingClient
     NumWorkers int
 }
 
+// result contains a collection of URLs from query, or error if query failed.
+type result struct {
+    collection *api.ImagesCollection
+    err error
+}
+
+// Crawl takes list of strings and send them (in parallel) the images search endpoint.
+// A result of each query represents a JSON object that is saved onto local disk with
+// exportFunc into outputFolder.
 func (c *Crawler) Crawl(queries []string, outputFolder string, exportFunc io.Exporter) {
     client := c.Client
     resultsQueue := make(chan result, 10)
     queriesQueue := make(chan string)
     utils.Check(os.MkdirAll(outputFolder, os.ModePerm))
 
-    go func(){
-        for _, query := range queries {
-            queriesQueue <- query
-        }
-        close(queriesQueue)
-    }()
+    go enqueueStrings(queries, queriesQueue)
 
     var workerGroup sync.WaitGroup
     for i := 1; i <= c.NumWorkers; i++ {
@@ -58,97 +63,34 @@ func (c *Crawler) Crawl(queries []string, outputFolder string, exportFunc io.Exp
     log.Printf("collected results are saved into folder: %s", outputFolder)
 }
 
-type result struct {
-    collection *api.ImagesCollection
-    err error
+// downloaded contains image URL and downloading success status.
+type downloaded struct {
+    url string
+    success bool
 }
 
-func queryWorker(
-    workerIndex int,
-    in <-chan string,
-    out chan<- result,
-    group *sync.WaitGroup,
-    client *api.BingClient) {
-
-    defer group.Done()
-
-    for queryString := range in {
-        if queryString == "" { continue }
-        log.Printf("[worker:%d] sending search string: %s", workerIndex, queryString)
-
-        currOffset := 0
-        running := true
-        var err error
-        for running {
-            params := api.CreateQuery(queryString, currOffset)
-            paramsString := params.AsQueryParameters()
-            log.Printf("[worker:%d] running query with params: %s", workerIndex, paramsString)
-            images := client.RequestImages(params)
-            if images.Values == nil {
-                err = fmt.Errorf("[worker:%d] failed to pull query: %s/%s",
-                    workerIndex, client.Endpoint, paramsString)
-                running = false
-            } else {
-                running = images.NextOffset != currOffset
-                currOffset = images.NextOffset
-            }
-            out <- result{images, err}
-        }
-    }
-
-    log.Printf("[worker:%d] terminated", workerIndex)
-}
-
-func writingWorker(
-    workerIndex int,
-    outputFolder string,
-    in <-chan result,
-    group *sync.WaitGroup,
-    exportFunc io.Exporter) {
-
-    defer group.Done()
-
-    for result := range in {
-        outputFile := path.Join(outputFolder, utils.SimpleRandomString(20))
-        log.Printf("[worker:%d] saving file %s", workerIndex, outputFile)
-        if result.err != nil {
-            log.Printf(result.err.Error())
-        } else {
-            query := result.collection.Query
-            log.Printf(
-                "[worker:%d] exporting query results for '%s' into file '%s",
-                workerIndex, query, outputFile)
-            if err := exportFunc(result.collection, outputFile); err != nil {
-                log.Printf(err.Error())
-            }
-        }
-    }
-
-    log.Printf("[worker:%d] terminated", workerIndex)
-}
-
+// Download takes previously retrieved queries results from metaDataFolder and starts
+// downloading them into imagesFolder. The importFunc is used to read queries files
+// from disk.
 func (c *Crawler) Download(metaDataFolder, imagesFolder string, importFunc io.Importer) {
     log.Printf("loading image URLs from folder: %s", metaDataFolder)
 
     utils.Check(os.MkdirAll(imagesFolder, os.ModePerm))
     urls, err := importFunc(metaDataFolder, "contentUrl")
-    if err != nil { log.Printf("%s", err) }
+    if err != nil{
+        log.Printf("%s", err)
+        return
+    }
 
     feed := make(chan string, c.NumWorkers)
-    go func () {
-        for _, url := range urls {
-            // log.Printf("submitting URL: %s", url)
-            feed <- url
-        }
-        close(feed)
-    }()
+    go enqueueStrings(urls, feed)
 
     log.Printf("launching workers...")
     var workerGroup sync.WaitGroup
     results := make(chan downloaded)
     for i := 1; i <= c.NumWorkers; i++ {
         workerGroup.Add(1)
-        go downloader(i, imagesFolder, feed, results, &workerGroup)
+        go downloadingWorker(i, imagesFolder, feed, results, &workerGroup)
     }
 
     go func(){
@@ -166,34 +108,4 @@ func (c *Crawler) Download(metaDataFolder, imagesFolder string, importFunc io.Im
     metaFile := path.Join(imagesFolder, "collected.json")
     _ = ioutil.WriteFile(metaFile, collectedJSON, os.ModePerm)
     log.Printf("collected results are saved into folder: %s", imagesFolder)
-}
-
-type downloaded struct {
-    url string
-    success bool
-}
-
-func downloader(
-    workerIndex int,
-    imagesFolder string,
-    urls <-chan string,
-    results chan<- downloaded,
-    group *sync.WaitGroup) {
-
-    defer group.Done()
-
-    fetcher := io.NewImageFetcher(1*time.Hour)
-    for url := range urls {
-        log.Printf("[worker:%d] fetching URL: %s", workerIndex, url)
-        outputFile := path.Join(imagesFolder, utils.SimpleRandomString(20))
-        success := true
-        err := fetcher.Fetch(url, outputFile)
-        if err != nil {
-            log.Printf("[worker:%d] %s", workerIndex, err.Error())
-            success = false
-        }
-        results <- downloaded{url, success}
-    }
-
-    log.Printf("[worker:%d] terminated", workerIndex)
 }
---
+++ crawler/workers.go
@@ -0,0 +1,113 @@
+package crawler
+
+import (
+    "bing/api"
+    "bing/io"
+    "bing/utils"
+    "fmt"
+    "log"
+    "path"
+    "sync"
+    "time"
+)
+
+// queryWorker performs REST API queries taking search strings from in channel, and saving
+// results into out channel.
+func queryWorker(
+    workerIndex int,
+    in <-chan string,
+    out chan<- result,
+    group *sync.WaitGroup,
+    client *api.BingClient) {
+
+    defer group.Done()
+
+    for queryString := range in {
+        if queryString == "" { continue }
+        log.Printf("[worker:%d] sending search string: %s", workerIndex, queryString)
+
+        currOffset := 0
+        running := true
+        var err error
+        for running {
+            params := api.CreateQuery(queryString, currOffset)
+            paramsString := params.AsQueryParameters()
+            log.Printf("[worker:%d] running query with params: %s", workerIndex, paramsString)
+            images := client.RequestImages(params)
+            if images.Values == nil {
+                err = fmt.Errorf("[worker:%d] failed to pull query: %s/%s",
+                    workerIndex, client.Endpoint, paramsString)
+                running = false
+            } else {
+                running = images.NextOffset != currOffset
+                currOffset = images.NextOffset
+            }
+            out <- result{images, err}
+        }
+    }
+
+    log.Printf("[worker:%d] terminated", workerIndex)
+}
+
+// writingWorker takes JSON structures from in channel and saves them onto disk.
+func writingWorker(
+    workerIndex int,
+    outputFolder string,
+    in <-chan result,
+    group *sync.WaitGroup,
+    exportFunc io.Exporter) {
+
+    defer group.Done()
+
+    for result := range in {
+        outputFile := path.Join(outputFolder, utils.SimpleRandomString(20))
+        log.Printf("[worker:%d] saving file %s", workerIndex, outputFile)
+        if result.err != nil {
+            log.Printf(result.err.Error())
+        } else {
+            query := result.collection.Query
+            log.Printf(
+                "[worker:%d] exporting query results for '%s' into file '%s",
+                workerIndex, query, outputFile)
+            if err := exportFunc(result.collection, outputFile); err != nil {
+                log.Printf(err.Error())
+            }
+        }
+    }
+
+    log.Printf("[worker:%d] terminated", workerIndex)
+}
+
+// downloadingWorker performs actual work of retrieving the images and saving them onto local disk.
+func downloadingWorker(
+    workerIndex int,
+    imagesFolder string,
+    urls <-chan string,
+    results chan<- downloaded,
+    group *sync.WaitGroup) {
+
+    defer group.Done()
+
+    fetcher := io.NewImageFetcher(1*time.Hour)
+    for url := range urls {
+        log.Printf("[worker:%d] fetching URL: %s", workerIndex, url)
+        outputFile := path.Join(imagesFolder, utils.SimpleRandomString(20))
+        success := true
+        err := fetcher.Fetch(url, outputFile)
+        if err != nil {
+            log.Printf("[worker:%d] %s", workerIndex, err.Error())
+            success = false
+        }
+        results <- downloaded{url, success}
+    }
+
+    log.Printf("[worker:%d] terminated", workerIndex)
+}
+
+// enqueueStrings sends strings into channel, and closes it.
+func enqueueStrings(strings []string, channel chan<- string) {
+    for _, item := range strings {
+        channel <- item
+    }
+    close(channel)
+}
